ad7703bafc3fce7da39ce49c9ecae4711794284d
中屋　渉 <70841740+nakayawataru@users.noreply.github.com>
2024-08-24 02:16:40 +0900

    kadai020
    kadai020teisyutu

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+const btn1 = document.getElementById('btn');
+
+btn1.addEventListener('click' , () => {
+
+  const change = document.querySelector('h2');
+
+  change.textContent = 'ボタンをクリックしました';
+
+} );
